c7feee18dabed628052efdb16fed684aaefc5ce7
AJ-Stilez <adenusijoseph0@gmail.com>
2026-04-17 13:51:39 +0100

    Add Dockerfile for Render deployment

--- obj/Debug/net10.0/CrudApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/CrudApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+208f4992c87d5cf9efd8d104d4d3824889f1f77d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
